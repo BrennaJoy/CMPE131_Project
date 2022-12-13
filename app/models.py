570fb3f24e8ac506363c59f07c3ae7f1dc281e3f
Koe <98497607+Sepchakoe@users.noreply.github.com>
2022-12-12 23:09:06 -0800

    adding class Posts

--- app/models.py
+++ app/models.py
@@ -22,3 +22,12 @@ class User(db.Model, UserMixin):
 def load_user(id):
     return User.query.get(int(id))
 
+#creating class post that user can post short message on homepage
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255))
+    content = db.Column(db.Text)
+    author = db.Column(db.String(255))
+    date_posted = db.Column(db.DateTime)
+
+
