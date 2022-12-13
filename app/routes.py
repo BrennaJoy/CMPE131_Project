f0563f5d4803b323532be659bb618db4546f8638
WilsonHamada99 <113465955+WilsonHamada99@users.noreply.github.com>
2022-12-12 20:11:32 -0800

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -154,6 +154,13 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-
+@myapp_obj.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': ''},
+    ]
+    return render_template('userprofile.html', user=user, posts=posts)
 
 
