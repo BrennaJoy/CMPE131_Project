340f81abeb215d98c47e25a9bf47fadcd9754189
WilsonHamada99 <113465955+WilsonHamada99@users.noreply.github.com>
2022-12-13 20:30:16 -0800

    Add files via upload

---
+++ app/Postmssg.py
@@ -0,0 +1,19 @@
+@myapp_obj.route('/postmessage', methods = ['POST','GET'])
+@login_required
+def post():
+    error = ''
+    current_form = PostForm()
+    if current_form.validate_on_submit():
+        if len(current_form.text.data) >250:
+            error = 'Post is too long! (max characters is 250)'
+        else:
+            post = Post()
+            post.post = current_form.text.data  #save body text to db
+            post.user_id = current_user.id      #save current user's id to track poster
+            today = date.today()
+            post.date = str(today)
+            with myapp_obj.app_context():       #add object to db
+                db.session.add(post)
+                db.session.commit()
+            return redirect('/homepage')
+    return render_template('postmessage.html', form = current_form, error = error)
